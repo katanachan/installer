643f0121f0ff25dd2c28429bf75431aca73a85fc
katanachan <shwarya.unnikrishnan@gmail.com>
2016-10-20 03:45:23 +0530

    Adding PYTHONPATH to .bashrc

--- VisualPythonInstaller.sh
+++ VisualPythonInstaller.sh
@@ -18,8 +18,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$SUDO_USER/.virtualenvs/workspace/
 cd wxPython
 CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security sudo python setup.py install
 python setup.py build
+echo "export PYTHONPATH="$PWD"" >> ~/.bashrc
 cd ../..
-deactivate
+deactivate 
 
 wget https://pypi.python.org/packages/dd/69/a6d5ba016f4e15a83e49471bcf91a7b8fbdf818e5acb002f554027d47650/TTFQuery-1.0.5.tar.gz
 tar -xvjf TTFQuery-1.0.5.tar.gz
