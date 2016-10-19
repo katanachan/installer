5cf6e176d95853cf9d67864cb1b1750d08f44041
katanachan <shwarya.unnikrishnan@gmail.com>
2016-10-19 22:39:34 +0530

    Update VisualPythonInstaller.sh

--- VisualPythonInstaller.sh
+++ VisualPythonInstaller.sh
@@ -14,14 +14,14 @@ CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security sudo pytho
 wget https://pypi.python.org/packages/dd/69/a6d5ba016f4e15a83e49471bcf91a7b8fbdf818e5acb002f554027d47650/TTFQuery-1.0.5.tar.gz
 tar -xvjf TTFQuery-1.0.5.tar.gz
 cd TTFQuery-1.0.5/
-python setup.py build && python setup.py install
+python setup.py build && sudo python setup.py install
 
 sudo pip install fonttools
 
 wget https://pypi.python.org/packages/73/9e/fe761e03de28b51b445ddf01ddae87441b7e7040df7d830b86db8f945808/Polygon2-2.0.8.tar.gz#md5=3349a6dfc4cda2a1bcc9bf6c9d411470
 tar -xvjf Polygon2-2.0.8.tar.gz
 cd Polygon2-2.0.8/
-python setup.py build && python setup.py install
+python setup.py build && sudo python setup.py install
 
 sudo apt-get update
 sudo apt-get install git
@@ -37,5 +37,5 @@ sudo apt-get install tk
 
 wget http://sourceforge.net/projects/vpythonwx/files/6.11-release/vpython-wx-src.6.11.tgz/download
 cd vpython-wx-src.6.11/
-python setup.py build && python setup.py install
+python setup.py build && sudo python setup.py install
 
