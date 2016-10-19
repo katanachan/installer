7c6248ff47970505989879047c1347be2db1dd8f
katanachan <shwarya.unnikrishnan@gmail.com>
2016-10-19 19:18:37 +0530

    Bash script to install Visual Python and its dependencies

---
+++ VisualPythonInstaller.sh
@@ -0,0 +1,41 @@
+sudo apt-get install libwxgtk3.0-dev
+sudo apt-get install virtualenv
+wget http://downloads.sourceforge.net/wxpython/wxPython-src-3.0.2.0.tar.bz2
+tar -xvjf wxPython-src-3.0.2.0.tar.bz2
+mkvirtualenv workspace	
+workon workspace
+cd wxPython-src-3.0.2.0/
+./configure –prefix=~/.virtualenvs/workspace –with-gtk2 –enable-unicode –with-opengl
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.virtualenvs/workspace/lib
+cd wxPython
+python setup.py build
+CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security sudo python setup.py install
+
+wget https://pypi.python.org/packages/dd/69/a6d5ba016f4e15a83e49471bcf91a7b8fbdf818e5acb002f554027d47650/TTFQuery-1.0.5.tar.gz
+tar -xvjf TTFQuery-1.0.5.tar.gz
+cd TTFQuery-1.0.5/
+python setup.py build && python setup.py install
+
+pip install fonttools
+
+wget https://pypi.python.org/packages/73/9e/fe761e03de28b51b445ddf01ddae87441b7e7040df7d830b86db8f945808/Polygon2-2.0.8.tar.gz#md5=3349a6dfc4cda2a1bcc9bf6c9d411470
+tar -xvjf Polygon2-2.0.8.tar.gz
+cd Polygon2-2.0.8/
+python setup.py build && python setup.py install
+
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libgtkglextmm-x11-1.2-dev
+sudo apt-get install libgtkmm-2.4-dev
+sudo apt-get install python-dev
+sudo apt-get install python-setuptools
+sudo apt-get install python-numpy
+sudo apt-get install libboost-python-dev
+sudo apt-get install libboost-signals-dev
+sudo apt-get install tk
+
+wget http://sourceforge.net/projects/vpythonwx/files/6.11-release/vpython-wx-src.6.11.tgz/download
+cd vpython-wx-src.6.11/
+python setup.py build && python setup.py install
+
