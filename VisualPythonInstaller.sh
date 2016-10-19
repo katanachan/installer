02cc7f1e331a250937bee8d291699bd880e57887
katanachan <shwarya.unnikrishnan@gmail.com>
2016-10-20 01:07:07 +0530

    Bugs updated

--- VisualPythonInstaller.sh
+++ VisualPythonInstaller.sh
@@ -16,16 +16,15 @@ cd wxPython-src-3.0.2.0/
 ./configure –prefix=~/.virtualenvs/workspace –with-gtk2 –enable-unicode –with-opengl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.virtualenvs/workspace/lib
 cd wxPython
-python setup.py build
+CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security python setup.py build
 CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security sudo python setup.py install
 cd ..
 
 wget https://pypi.python.org/packages/dd/69/a6d5ba016f4e15a83e49471bcf91a7b8fbdf818e5acb002f554027d47650/TTFQuery-1.0.5.tar.gz
 tar -xvjf TTFQuery-1.0.5.tar.gz
 cd TTFQuery-1.0.5/
-python setup.py build && python setup.py install
-cd ..
 python setup.py build && sudo python setup.py install
+cd..
 
 sudo pip install fonttools
  
