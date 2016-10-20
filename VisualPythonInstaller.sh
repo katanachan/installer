fc39abcf8529e2ea5e31373133f72d4e0547ce1a
katanachan <shwarya.unnikrishnan@gmail.com>
2016-10-20 07:18:37 +0530

    Changes

--- VisualPythonInstaller.sh
+++ VisualPythonInstaller.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -
+#!/bin/sh
 #title       : vpythonlinuxinstall.sh
 #description : A shell script to install VPython on Linux 
 #author      : Aishwarya Unnikrishnan (github.com/katanachan)
@@ -8,22 +8,24 @@
 
 sudo apt-get install libwxgtk3.0-dev
 sudo apt-get install virtualenv
-wget http://downloads.sourceforge.net/wxpython/wxPython-src-3.0.2.0.tar.bz2
-tar -xvjf wxPython-src-3.0.2.0.tar.bz2
+wget http://downloads.sourceforge.net/wxpython/wxPython-src-3.0.1.0.tar.bz2
+tar -xvjf wxPython-src-3.0.1.0.tar.bz2
+
+source `which virtualenvwrapper.sh`
 mkvirtualenv workspace	
 workon workspace
-cd wxPython-src-3.0.2.0/
-./configure --prefix=/home/$SUDO_USER/.virtualenvs/workspace/lib/ --with-gtk2 --enable-unicode --with-opengl
+cd wxPython-src-3.0.1.0/
+./configure --prefix=/home/$SUDO_USER/.virtualenvs/workspace/lib/ --with-gtk2 --enable-unicode --with-opengl --enable-stl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$SUDO_USER/.virtualenvs/workspace/lib
 cd wxPython
+echo "export PYTHONPATH="$PWD"" >> ~/.bashrc
 CFLAGS=-Wno-error=format-security CPPFLAGS=-Wno-error=format-security sudo python setup.py install
 python setup.py build
-echo "export PYTHONPATH="$PWD"" >> ~/.bashrc
 cd ../..
-deactivate 
+deactivate
 
 wget https://pypi.python.org/packages/dd/69/a6d5ba016f4e15a83e49471bcf91a7b8fbdf818e5acb002f554027d47650/TTFQuery-1.0.5.tar.gz
-tar -xvjf TTFQuery-1.0.5.tar.gz
+tar -xvzf TTFQuery-1.0.5.tar.gz
 cd TTFQuery-1.0.5/
 python setup.py build && sudo python setup.py install
 cd ..
@@ -31,7 +33,7 @@ cd ..
 sudo pip install fonttools
  
 wget https://pypi.python.org/packages/73/9e/fe761e03de28b51b445ddf01ddae87441b7e7040df7d830b86db8f945808/Polygon2-2.0.8.tar.gz#md5=3349a6dfc4cda2a1bcc9bf6c9d411470
-tar -xvjf Polygon2-2.0.8.tar.gz
+tar -xvzf Polygon2-2.0.8.tar.gz
 cd Polygon2-2.0.8/
 python setup.py build && sudo python setup.py install
 cd ..
@@ -48,6 +50,7 @@ sudo apt-get install libboost-python-dev
 sudo apt-get install libboost-signals-dev
 sudo apt-get install tk
 
-wget http://sourceforge.net/projects/vpythonwx/files/6.11-release/vpython-wx-src.6.11.tgz/download
+wget http://sourceforge.net/projects/vpythonwx/files/6.11-release/vpython-wx-src.6.11.tgz
+tar -xvzf vpython-wx-src.6.11.tgz
 cd vpython-wx-src.6.11/
 python setup.py build && sudo python setup.py install
